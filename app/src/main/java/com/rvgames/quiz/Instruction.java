856d1e956e0ce3da3399bb7fa7141ea3f6dc0c9e
vedpatil611 <vedpatil611@gmail.com>
2019-03-05 19:21:15 +0530

    Instructions

--- app/src/main/java/com/rvgames/quiz/Instruction.java
+++ app/src/main/java/com/rvgames/quiz/Instruction.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Instruction extends AppCompatActivity {
@@ -11,18 +12,21 @@ public class Instruction extends AppCompatActivity {
     int i;
     String[] rule;
     TextView rules;
-
+    Button previous, next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+        previous = findViewById(R.id.button34);
+        next = findViewById(R.id.button36);
         rules = findViewById(R.id.textViewRule);
         rule = new String[3];
         rule[0] = "\tIn difficulty easy there is no time limit.\n\n\tFor every correct answer you will be awarded 20 points.\n\n\tThere is no negative marking.";
         rule[1] = "\tIn difficulty medium there is time limit of 5 seconds per question.\n\n\tFor every correct answer you will be awarded 20 points.\n\n\tThere is no negative marking.";
-        rule[2] = "\tIn difficulty medium there is time limit of 5 seconds per question.\n\n\tFor every correct answer you will be awarded 20 points.\n\n\tFor every wrong answer 20 points will be reduced.";
+        rule[2] = "\tIn difficulty hard there is time limit of 5 seconds per question.\n\n\tFor every correct answer you will be awarded 20 points.\n\n\tFor every wrong answer 10 points will be reduced.";
         i = 0;
         rules.setText(rule[i]);
+        previous.setVisibility(View.INVISIBLE);
     }
 
     public void pressNext(View view) {
@@ -30,12 +34,24 @@ public class Instruction extends AppCompatActivity {
             i++;
             rules.setText(rule[i]);
         }
+        if (i > 0) {
+            previous.setVisibility(View.VISIBLE);
+        }
+        if(i == 2) {
+            next.setVisibility(View.INVISIBLE);
+        }
     }
 
     public void pressPrevious(View view) {
         if (i > 0) {
             i--;
             rules.setText(rule[i]);
+        }
+        if(i < 2) {
+            next.setVisibility(View.VISIBLE);
+        }
+        if(i == 0) {
+            previous.setVisibility(View.INVISIBLE);
         }
     }
 
